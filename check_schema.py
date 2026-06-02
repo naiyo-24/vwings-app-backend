3f43549fb430de03cf21ed9afc023c280f21c409
Sayar-2121 <sayarpaul20004@gmail.com>
2026-06-02 12:54:39 +0530

    refactor: remove obsolete migration scripts and utility files

--- check_schema.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect("d:\\VWings24x7-App-Backend\\vwings.db")
-cursor = conn.cursor()
-
-cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
-print("Tables:", cursor.fetchall())
-conn.close()
